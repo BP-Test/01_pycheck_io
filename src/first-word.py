a06d05f824a9db9939da1488b453e8f383bf7167
BP-Test <tatsuro.0315@keio.jp>
2021-09-11 01:04:50 +0900

    Add my answer for first-word

---
+++ src/first-word.py
@@ -0,0 +1,6 @@
+def first_word(text: str) -> str:
+    """
+        returns the first word in a given text.
+    """
+    list_text = re.findall(r"[a-zA-Z']+",text) # Remove characters that are not [a-zA-Z']
+    return list_text[0]
